19d947a0e8b237a693a9182eb475b9c063763190
Abhishek <omg.gorakh@gmail.com>
2023-09-24 22:50:53 +0530

    Creating api post connecting with the mongo immediate fix 3

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 #Stopping existing node servers
+sudo chmod -R 777 /home/ec2-user/express-app
 echo "Stopping any existing node servers"
+# Check if the process exists before attempting to kill it
+
+
 pkill node
